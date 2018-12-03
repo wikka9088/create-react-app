c547b9f4bbc4ea5de28b8902d42c96cf5caaf4d9
wikka9088 <wikka9088@gmail.com>
2018-12-02 23:25:57 -0500

    create first class component

--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import HelloWorld from './components/HelloWorld';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(<HelloWorld/>, document.getElementById('helloWorld'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
